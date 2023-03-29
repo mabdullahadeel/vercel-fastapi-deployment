88c923e5ae2a6afabdcd3f3b1c886838c9d5b5aa
Abdullah Adeel <easyautosol@gmail.com>
2023-03-29 16:01:51 +0500

    🗃️ serve static files

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, __version__
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <h1>Hello from FastAPI@{__version__} on Vercel</h1>
+    </body>
+</html>
+"""
 
 @app.get("/")
 async def root():
-    return HTMLResponse("Hello from fastAPI on vercel")
+    return HTMLResponse(html)
